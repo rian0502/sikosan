3824487e52a7ea9dc307cd4f844d55dec58d6015
Rifan Setiadi <rfan2442@gmail.com>
2022-10-08 21:33:46 +0700

    Update landing page, navbar, dummy file, dan template

---
+++ app/Views/partials/about.php
@@ -0,0 +1,35 @@
+<section class="mt-5">
+    <h2>Sikosan - Temukan Kos di Lampung</h2>
+    <div class="row">
+        <div class="col">
+            <p>Sikosan merupakan website yang membantu pengguna untuk mencari kos-kosan di Lampung. Sikosan menjawab keresahan pencari kos yang kesulitan terutama bagi mereka yang berasal dari luar Provinsi Lampung. Pengguna dapat memilih kos-kosan sesuai keinginan baik berdasarkan harga, lokasi, dan juga kelengkapan fasilitas yang tersedia.</p>
+            <p>Dengan memanfaatkan API yang tersedia, Sikosan menyediakan berbagai pilihan kos yang tersedia di berbagai kabupaten dan kecamatan yang ada di Provinsi Lampung.</p>
+
+            <section>
+                <h3>Fitur Sikosan</h3>
+                <div class="">
+                    <h5>Filter Pencarian</h5>
+                    <p>Cari kos-kosan berdasarkan lokasi yang kamu inginkan, kami akan memberikan saran-saran kos yang dapat kamu pilih untuk memudahkan pemilihan kos-kosan di area tertentu.</p>
+                </div>
+                <div class="">
+                    <h5>Komentar</h5>
+                    <p>Berikan komentarmu terhadap pengalaman kamu tinggal di kos yang kamu pilih saat ini atau terdahulu. Berikan saran terbaikmu di kolom komentar agar pengguna lain terbantu untuk memilih kos.</p>
+                </div>
+            </section>
+        </div>
+        <div class="col">
+            <div class="">
+                <h5>Booking</h5>
+                <p>Segera booking kos yang kamu pilih agar tidak lebih dulu di "keep" oleh pengguna lain. Booking kos akan memberikan pesan kepada owner kos bahwa kamu berminat dan bersedia membayar uang muka untuk kos tersebut.</p>
+            </div>
+            <div class="">
+                <h5>Report Postingan Kos</h5>
+                <p>Jangan khawatir j ika tidak terdapat kesesuaian pada kos yang kamu pilih berdasarkan detail pada website Sikosan. Laporkan keluhan terhadap kos yang kamu tempati. Admin akan segera memproses keluhan yang kamu ajukan.</p>
+            </div>
+            <div class="">
+                <h5>Favorit</h5>
+                <p>Kamu tertarik dengan kos tertentu? Tapi kamu belum ingin melakukan booking? Tambahkan kos tersebut ke dalam list favoritmu supaya kamu tidak kehilangan postingan kos yang kamu sukai.</p>
+            </div>
+        </div>
+    </div>
+</section>
