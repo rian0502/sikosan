7ac80809fa87885092138be84875b4b2fba2c30c
Rifan Setiadi <rfan2442@gmail.com>
2022-10-08 17:53:03 +0700

    Membuat Landing Page dan Template Navbar

---
+++ app/Views/templates/template.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIKOSAN</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/assets/customcss/style.css">
+
+</head>
+
+<body>
+    <!-- Menggunakan file navbar -->
+    <div class="container-fluid">
+        <?= $this->include('templates/navbar'); ?>
+    </div>
+    <!-- Section -->
+    <div class="container mt-4">
+        <?= $this->renderSection('content'); ?>
+    </div>
+
+</body>
+
+
+<script src="/jquery/jquery.min.js"></script>
+<script src="/bootstrap/js/bootstrap.bundle.js"></script>
+
+</html>
