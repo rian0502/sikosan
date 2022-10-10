23047b4d994dacd389274ead46799832662fcbe3
Rifan Setiadi <rfan2442@gmail.com>
2022-10-10 21:21:35 +0700

    Update routes, controller, dan mengarahkan landing page ke detail

--- app/Views/dummy_files/dummy_list_kos.php
+++ app/Views/dummy_files/dummy_list_kos.php
@@ -9,7 +9,7 @@
                 </div>
                 <h5 class="card-title mt-2">Kosan 4 Pasar Rabu</h5>
                 <p class="card-text">Kecamatan Kemiling, Desa Bandar Baru.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -23,7 +23,7 @@
                 </div>
                 <h5 class="card-title mt-2">Wisma Andini</h5>
                 <p class="card-text">Kecamatan Kemiling, Desa Talang Barat.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -37,7 +37,7 @@
                 </div>
                 <h5 class="card-title mt-2">Wisma Istiqomah</h5>
                 <p class="card-text">Kecamatan Labuan Ratu, Desa Semungkur.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -51,7 +51,7 @@
                 </div>
                 <h5 class="card-title mt-2">Asrama Sari</h5>
                 <p class="card-text">Kecamatan Rajabasa, Desa Rajabasa.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -67,7 +67,7 @@
                 </div>
                 <h5 class="card-title mt-2">Wisma Sanabil</h5>
                 <p class="card-text">Kecamatan Sukarame, Desa Sukamaju.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -81,7 +81,7 @@
                 </div>
                 <h5 class="card-title mt-2">Kosan Damai</h5>
                 <p class="card-text">Kecamatan Kedaton, Desa Ciapus.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -95,7 +95,7 @@
                 </div>
                 <h5 class="card-title mt-2">Queen Kos</h5>
                 <p class="card-text">Kecamatan Sukabumi, Desa Sukahir.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
@@ -109,7 +109,7 @@
                 </div>
                 <h5 class="card-title mt-2">Kos Batara</h5>
                 <p class="card-text">Kecamatan Teluk Betung, Desa Mawar.</p>
-                <a href="/" class="btn btn-outline-success">Detail</a>
+                <a href="/detail" class="btn btn-outline-success">Detail</a>
             </div>
         </div>
     </div>
