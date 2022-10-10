9e6b5fa9f68d744ee62fe42425f51f90e0cfaee1
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-10-10 13:26:13 +0700

    memperbaiki view login register, melakukan routing

--- app/Views/owners/login.php
+++ app/Views/owners/login.php
@@ -1,33 +1,42 @@
 <?= $this->extend('templates/templateAuth') ?>
 
 <?= $this->section('content') ?>
+<div id="auth">
+    <div class="row h-100">
+        <div class="col-lg-5 col-12">
+            <div id="auth-left">
+                <div class="auth-logo">
+                    <a href="/"><img src="/assets/img/sikosan.png" alt="Logo"></a>
+                </div>
+                <h1 class="auth-title">Login.</h1>
+                <p class="auth-subtitle mb-5">Masuk Sebagai Pemilik Kost.</p>
 
-<div class="row">
-
-    <div class="col-lg-4 ms-3">
-        <div class="col">
-            <a href="#" class="rounded-circle">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <i class="bi bi-arrow-left-circle-fill" style="font-size: 2rem; color: #000000;"></i>
+                <form action="index.html">
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="text" class="form-control form-control-xl" placeholder="Username">
+                        <div class="form-control-icon">
+                            <i class="bi bi-person"></i>
                         </div>
                     </div>
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="password" class="form-control form-control-xl" placeholder="Password">
+                        <div class="form-control-icon">
+                            <i class="bi bi-shield-lock"></i>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                </form>
+                <div class="text-center mt-5 text-lg fs-4">
+                    <p class="text-gray-600">Tidak mempunyai akun? <a href="/registerOwner" class="font-bold">Daftar</a>.</p>
+                    <p><a class="font-bold" href="auth-forgot-password.html">Lupa Password?</a>.</p>
                 </div>
-            </a>
-            <h2 class="mt-4">Login Pemilik Kost</h2>
+            </div>
         </div>
-        <form>
-            <div class="mb-4 mt-5">
-                <label for="email" class="form-label">Email address</label>
-                <input name="email" type="email" class="form-control" id="email">
+        <div class="col-lg-7 d-none d-lg-block">
+            <div id="auth-right">
+
             </div>
-            <div class="mb-4">
-                <label for="password" class="form-label">Password</label>
-                <input name="password" type="password" class="form-control" id="password">
-            </div>
-            <button type="submit" class="btn btn-primary">Masuk</button>
-        </form>
+        </div>
     </div>
 </div>
 <?= $this->endSection(); ?>
