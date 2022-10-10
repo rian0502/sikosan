494e860138a3f245a0f1138a3395ccfa885776bd
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-10-10 13:26:13 +0700

    memperbaiki view login register, melakukan routing

--- app/Views/templates/templateAuth.php
+++ app/Views/templates/templateAuth.php
@@ -5,7 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SIKOSAN</title>
-
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/adminTemplate/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="/adminTemplate/assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="/adminTemplate/assets/css/app.css">
+    <link rel="stylesheet" href="/adminTemplate/assets/css/pages/auth.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
 
@@ -21,14 +25,6 @@
 <script src="/jquery/jquery.min.js"></script>
 <script src="/bootstrap/js/bootstrap.bundle.js"></script>
 <script>
-    $('#ulangi-password').change(function(e) {
-        e.preventDefault();
-        if ($('#password').val() != $('#ulangi-password').val()) {
-            $('#ulangi-password').addClass('is-invalid');
-        } else {
-            $('#ulangi-password').removeClass('is-invalid');
-        }
-    });
 </script>
 
 </html>
