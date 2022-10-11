ccc51fbc54bccb0dd4294110baaa042546a10920
YOGI ANDARU <yogiandaru98@gmail.com>
2022-10-11 15:10:19 +0700

    make landing page responsive

--- app/Views/dummy_files/dummy_list_kos.php
+++ app/Views/dummy_files/dummy_list_kos.php
@@ -4,7 +4,7 @@
             width: 18rem;
         }
     }
-    @media only screen and (max-width: 600px) {
+    @media only screen and (max-width: 768px) {
         .card-img-top{
             height:auto;
         }
