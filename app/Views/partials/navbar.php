19f15e82782eea7b52883925f937327f158ca155
Rifan Setiadi <rfan2442@gmail.com>
2022-10-14 20:43:10 +0700

    ganti nama login

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -22,49 +22,10 @@
                 </li>
                 <li class="nav-item">
                     <a href="<?= url_to('login'); ?>" class="btn btn-outline-primary ms-2">
-                        Celup
+                        Login
                     </a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Masuk Sikosan</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <a class="text-decoration-none text-dark" href="<?= base_url('/customer'); ?>">
-                    <div class="card pt-2 pb-2" style="width: 100%;">
-                        <div class="row">
-                            <div class="col-4">
-                                <img src="/assets/img/customer-login.png" class="" style="width: 90px;">
-                            </div>
-                            <div class="col-8 m-auto">
-                                <p class="card-text fw-bold">Pencari Kos</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-
-                <a class="text-decoration-none text-dark" href="<?= base_url('/owner'); ?>">
-                    <div class="card pt-2 pb-2 mt-2" style="width: 100%;">
-                        <div class="row">
-                            <div class="col-4">
-                                <img src="/assets/img/owner-login.png" class="" style="width: 90px;">
-                            </div>
-                            <div class="col-8 m-auto">
-                                <p class="card-text fw-bold">Pemilik Kos</p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-
-            </div>
-        </div>
-    </div>
