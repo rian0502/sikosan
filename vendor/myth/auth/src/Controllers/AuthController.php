4fccc86468c72219da678af610e3c18561bd58ad
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-10-14 19:50:05 +0700

    konfigurasi form untuk mythAuth

--- vendor/myth/auth/src/Controllers/AuthController.php
+++ vendor/myth/auth/src/Controllers/AuthController.php
@@ -147,7 +147,6 @@ class AuthController extends Controller
 
         // Validate basics first since some password rules rely on these fields
         $rules = config('Validation')->registrationRules ?? [
-            'username' => 'required|alpha_numeric_space|min_length[3]|max_length[30]|is_unique[users.username]',
             'email'    => 'required|valid_email|is_unique[users.email]',
         ];
 
--- vendor/myth/auth/src/Models/UserModel.php
+++ vendor/myth/auth/src/Models/UserModel.php
@@ -17,15 +17,18 @@ class UserModel extends Model
     protected $returnType     = User::class;
     protected $useSoftDeletes = true;
     protected $allowedFields  = [
-        'email', 'username', 'password_hash', 'reset_hash', 'reset_at', 'reset_expires', 'activate_hash',
+        'namaLengkap','notlp', 'email','username', 'password_hash', 'reset_hash', 'reset_at', 'reset_expires', 'activate_hash',
         'status', 'status_message', 'active', 'force_pass_reset', 'permissions', 'deleted_at',
     ];
     protected $useTimestamps   = true;
+
     protected $validationRules = [
         'email'         => 'required|valid_email|is_unique[users.email,id,{id}]',
-        'username'      => 'required|alpha_numeric_punct|min_length[3]|max_length[30]|is_unique[users.username,id,{id}]',
+        'namaLengkap' => 'required|min_length[3]',
+        'notlp' => 'required|min_length[10]|is_numeric',
         'password_hash' => 'required',
     ];
+
     protected $validationMessages = [];
     protected $skipValidation     = false;
     protected $afterInsert        = ['addToGroup'];
