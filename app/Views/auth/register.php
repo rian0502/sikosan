44d91f3ec83ff8ab268c3d2f1049a9fda01360ea
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-10-15 08:27:56 +0700

    setting

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -48,6 +48,13 @@
                             <i class="bi bi-shield-lock"></i>
                         </div>
                     </div>
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <select class="form-select" aria-label="Default select example" name="group_id">
+                        
+                            <option value="owner">Owner</option>
+                            <option value="customer">Customer</option>
+                        </select>
+                    </div>
                     <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Sign Up</button>
                 </form>
                 <div class="text-center mt-5 text-lg fs-4">
