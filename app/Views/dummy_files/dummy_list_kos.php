2ba6b197956415b801a41d9fe78b7c759c45da94
YOGI ANDARU <yogiandaru98@gmail.com>
2022-10-16 17:32:45 +0700

    nampilin data dari database

--- app/Views/dummy_files/dummy_list_kos.php
+++ app/Views/dummy_files/dummy_list_kos.php
@@ -21,7 +21,7 @@
     }
 </style>
 <div class="row mb-3">
-    <<?php foreach ($kosan as $dataKos) : ?>
+    <?php foreach ($kosan as $dataKos) : ?>
      
     <div class="col-lg-3 mb-4 col-md-4 col-sm-6">
         <div class="card shadow" style="height: 420px;" name="kartu">
@@ -38,5 +38,4 @@
         </div>
     </div>
     <?php endforeach ?>
-    
 </div>
