2b3f97eecbb12bffefa5d0c195b41efb4be8d174
Rifan Setiadi <rfan2442@gmail.com>
2022-10-22 15:52:24 +0700

    Menambahkan method pada model untuk mengambil data kosan : - Berdasarkan id kosan, id user, dan all data kosan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -54,6 +54,7 @@ $routes->get('/admin/data_laporan', 'LaporanController::index', ['filter' => 'ro
 // For Penyewa Kos
 $routes->get('/owner/halaman_pemilik', 'OwnerController::halaman_pemilik', ['filter' => 'role:owner']);
 $routes->get('/owner/kosan_anda', 'OwnerController::kosan_anda', ['filter' => 'role:owner']);
+$routes->get('/owner/detail_kosan_anda/(:any)', 'OwnerController::detail_kosan_anda/$1', ['filter' => 'role:owner']);
 $routes->get('/owner/tambah_kosan', 'KosanController::create', ['filter' => 'role:owner']);
 $routes->post('/owner/save_kosan', 'KosanController::save', ['filter' => 'role:owner']);
 $routes->delete('/owner/delete_kosan/(:any)', 'KosanController::delete/$1', ['filter' => 'role:owner']);
--- app/Controllers/OwnerController.php
+++ app/Controllers/OwnerController.php
@@ -31,6 +31,19 @@ class OwnerController extends BaseController
         return view('auth/owner/kosan_anda_page', $data);
     }
 
+    public function detail_kosan_anda($id_kosan)
+    {
+        $kosanModel = new KosanModel();
+        $kosan = $kosanModel->getKosanByIdKosan($id_kosan);
+
+        $data = [
+            'title' => 'Detail Kosan Anda | Owner',
+            'kosan' => $kosan
+        ];
+
+        return view('auth/owner/detail_kosan_anda_page', $data);
+    }
+
     public function profil()
     {
         return view('auth/join/profil_page');
