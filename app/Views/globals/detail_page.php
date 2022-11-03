d807842317de08cdec89b4cbc9942956fbc37cfc
YOGI ANDARU <yogiandaru98@gmail.com>
2022-11-03 17:47:11 +0700

    darkmode owner

--- app/Views/globals/detail_page.php
+++ app/Views/globals/detail_page.php
@@ -148,7 +148,7 @@
 </section>
 
 <!-- Related items section-->
-<section class="py-5 bg-light">
+<section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
         <h4 class="fw-bolder mb-4">Kos serupa yang mungkin kamu cari</h4>
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
