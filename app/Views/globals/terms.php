7cca3533ba0f5b4eb02a9e009541e5581ed75766
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2022-11-14 15:47:16 +0700

    memperbaiki halaman login

--- app/Views/globals/terms.php
+++ app/Views/globals/terms.php
@@ -2,4 +2,11 @@
 
 <?= $this->section('content'); ?>
 <h1>Terms & Conditions</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
