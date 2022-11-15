4115077f431516f55c32b58a789553f5c0b47ad5
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2022-11-15 09:26:43 +0700

    tambah ilustrasi dikit bagian terms

--- app/Views/globals/terms.php
+++ app/Views/globals/terms.php
@@ -3,7 +3,7 @@
 <?= $this->section('content'); ?>
 
 <div class="container">
-    <div class="row">
+    <div class="row mb-5">
         <div id="ilustrasi" class="col-5 mt-5">
             <img src="/assets/img/terms_ilustrasi.svg" alt="">
         </div>
@@ -16,11 +16,10 @@
 
                 Website ini dimiliki oleh sikosan dan afiliasinya (selanjutnya disebut, “Kami”). Kami menyarankan pendatang, pengguna, ataupun pengguna terdaftar (selanjutnya disebut “Anda”) untuk membaca Syarat dan Ketentuan ini secara berkala termasuk juga Kebijakan Privasi dan Kebijakan Penalti Pengguna yang merupakan bagian yang tidak terpisahkan dari Syarat dan Ketentuan ini karena dapat berdampak kepada hak dan kewajiban Anda di bawah hukum.
 
-                Dengan mengunjungi, menggunakan, mengakses dan/atau mendaftarkan diri Anda pada Platform Kami, Anda dianggap telah membaca, mengerti, memahami dan menyetujui seluruh isi yang tertuang dalam Syarat dan Ketentuan (selanjutnya disebut, “S&K”) ini. Jika Anda tidak setuju untuk terikat dengan S&K in maka Anda tidak diperkenankan untuk mengakses dan/atau menggunakan Platform Kami. S&K ini merupakan bentuk Perjanjian yang sah dan mengikat antara Anda dengan Kami.
             </p>
 
-            <div class="h1">
-                Definisi
+            <div class="h3">
+                Penyewa Kos
             </div>
 
             <ol type="1">
@@ -29,15 +28,42 @@
                 <li>“Akun User Mamikos” berarti akun yang terdaftar milik Anda dan dikelola Mamikos.</li>
                 <li>“Check-in” Penghuni datang dan tinggal di Properti untuk mulai tinggal pada unit kamar yang telah dipesan.</li>
                 <li>“Afiliasi” berarti (i) setiap individu yang mewakili atau yang berkuasa atas nama Pihak lainnya; (ii) setiap perusahaan atau badan lainnya yang memiliki kendali atas salah satu Pihak, (iii) setiap perusahaan atau badan lainnya yang mana salah satu Pihak memiliki kendali atas perusahaan atau badan lainnya tersebut atau (iv) setiap perusahaan atau badan lainnya yang berada di bawah kendali yang sama dengan salah satu Pihak.</li>
-                <li>“Ajukan Sewa” berarti Penghuni dapat melakukan pemesanan dan/atau mengajukan sewa pada properti untuk tanggal yang tersedia dan dipilih oleh Penghuni.</li>
-                <li>“Akun User Mamikos” berarti akun yang terdaftar milik Anda dan dikelola Mamikos.</li>
-                <li>“Check-in” Penghuni datang dan tinggal di Properti untuk mulai tinggal pada unit kamar yang telah dipesan.</li>
+
+            </ol>
+
+            <!-- <div class="h3">
+                Pencari Kos
+            </div> -->
+
+        </div>
+
+    </div>
+
+    <!-- pemisah -->
+
+    <div class="row mt-7">
+
+        <div id="ilustrasi" class="col-7 mt-5">
+
+            <div class="h3">
+                Pencari Kos
+            </div>
+
+            <ol type="1">
                 <li>“Afiliasi” berarti (i) setiap individu yang mewakili atau yang berkuasa atas nama Pihak lainnya; (ii) setiap perusahaan atau badan lainnya yang memiliki kendali atas salah satu Pihak, (iii) setiap perusahaan atau badan lainnya yang mana salah satu Pihak memiliki kendali atas perusahaan atau badan lainnya tersebut atau (iv) setiap perusahaan atau badan lainnya yang berada di bawah kendali yang sama dengan salah satu Pihak.</li>
                 <li>“Ajukan Sewa” berarti Penghuni dapat melakukan pemesanan dan/atau mengajukan sewa pada properti untuk tanggal yang tersedia dan dipilih oleh Penghuni.</li>
                 <li>“Akun User Mamikos” berarti akun yang terdaftar milik Anda dan dikelola Mamikos.</li>
                 <li>“Check-in” Penghuni datang dan tinggal di Properti untuk mulai tinggal pada unit kamar yang telah dipesan.</li>
+                <li>“Afiliasi” berarti (i) setiap individu yang mewakili atau yang berkuasa atas nama Pihak lainnya; (ii) setiap perusahaan atau badan lainnya yang memiliki kendali atas salah satu Pihak, (iii) setiap perusahaan atau badan lainnya yang mana salah satu Pihak memiliki kendali atas perusahaan atau badan lainnya tersebut atau (iv) setiap perusahaan atau badan lainnya yang berada di bawah kendali yang sama dengan salah satu Pihak.</li>
+
             </ol>
+
         </div>
+        <div class="col-5">
+            <img src="/assets/img/terms2_ilustrasi.svg" alt="">
+        </div>
+
     </div>
+
 </div>
 <?= $this->endSection(); ?>
